b6b0972edc540aacd03459dbd8dff84bf1d00579
tiffany <tiffany@192.168.0.10>
2023-07-31 21:09:39 -0400

    added method - tiffany

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -4,16 +4,23 @@ public class HWDemo {
 
 	public static void main(String[] args) {
 		System.out.println("HW");
-		
+
 		System.out.println("*******Eltaf Sameem*******");
-		
+
 		System.out.println("*******Swen Gonzalez*******");
-		
+
 		System.out.println("*******Aisha Aljanabi*******");
 
 	}
+
 	protected boolean random(int m) {
 
 		return false;
+	}
+
+	public static void add(int a, int b) {
+		
+		System.out.println(a + b);
+
 	}
 }
